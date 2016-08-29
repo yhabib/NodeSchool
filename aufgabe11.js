146a56efdf32efe0afc5a4d48efc3c0e4165e351
Yusef Habib <Yusef.Habib@fct.de>
2016-08-29 12:10:51 +0200

    New exercise

--- aufgabe11.js
+++ aufgabe11.js
@@ -1,18 +1,14 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = process.argv[2];
 const path = process.argv[3];
 
-const server = http.createServer(callback);
+const server = http.createServer((req, res) => {
+	res.writeHead(200, {'content-type': 'text/plain'});
+	fs.createReadStream(path).pipe(res);
+});
 
 server.listen(port);
-
-
-
-function callback(req, res) {
-	console.log(req);
-	console.log(res);
-}
-
 
 
